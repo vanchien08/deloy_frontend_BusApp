fd2c8757a9030b6874877bb8008128b7ce7b654b
vanchien08 <134078712+vanchien08@users.noreply.github.com>
2025-08-08 14:42:29 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 const config = {
   api: {
-    API_BASE_URL: "https://deloy-backend-busapp.onrender.com/api/bus-station/",
+    API_BASE_URL: "https://deloy-backend-busapp.onrender.com/",
     ROUTER_BASE_NAME: null,
   },
   app: {
@@ -10,4 +10,5 @@ const config = {
 };
 
 export default config;
+
 
